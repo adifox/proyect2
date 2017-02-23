6c400b9cc5f659c076d2c2b7159d32c7c69c1a54
EnrikeGonzaloLegarra <enrikeglegarra@gmail.com>
2017-02-23 16:48:03 +0100

    jueves23

--- models/user.js
+++ models/user.js
@@ -2,12 +2,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const userSchema = new Schema({
   name: String,
   email: String,
   password: String,
   adress: String,
-  type: {}
+  artist: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Profile'
+  }]
 });
 
 userSchema.set('timestamps', true);
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
+const Picture = require('../models/picture');
 const profile = require('../models/profile');
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -24,12 +25,14 @@ router.get('/:id/edit', (req, res, next) => {
 
   });
 });
-////////////LLEGA HASTA AQUI!!!!//////////////////////////////////////////
+////////////Condicion para saber si tiene artist activado!!!!//////////////////////////////////////////
 
 router.post('/:id', (req, res, next) => {
-
+  //CONDICION QUE COMPRUEBA SI ES ARTISTA O NO!!!
+  //  if (req.body.check !== true) {
   const id = req.params.id;
   console.log("ID -> " + id);
+  console.log("artist ---------->" + req.body.check);
   const body = req.body;
   const {
     name,
@@ -46,6 +49,7 @@ router.post('/:id', (req, res, next) => {
       adress
     }
   };
+
   User.updateOne(criteria, update, function(err, user) {
     if (err) return next(err);
     res.redirect('/profile');
@@ -66,6 +70,34 @@ router.get('/:id/delete', (req, res, next) => {
   });
 });
 
+router.get('/show-artist', (req, res, next) => {
+  User.find({}, function(err, user) {
+    if (err) return next(err);
+    res.render('user/show-users', {
+      user: user
+    });
+  });
+});
+
+router.get('/:id/details', (req, res, next) => {
+  const id = req.params.id;
+  User.findOne({
+    _id: id
+  }, function(err, user) {
+    if (err) return next(err);
+    Picture.find({
+      picUserID: user._id
+    }, function(err, pictures) {
+      console.log('pictures', pictures);
+      console.log('user', user);
+      if (err) return next(err);
+      res.render('user/details', {
+        user: user,
+        pictures: pictures
+      });
+    });
+  });
+});
 
 
 module.exports = router;
